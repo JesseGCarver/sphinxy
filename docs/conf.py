2fc669433984ea300b95774cf01e635c2d78c32b
Jesse G. Carver <carver@Jesses-MacBook-Pro.local>
2023-04-20 14:46:44 -0600

    theming, fave icon

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = 'Jesse Carver'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    ]
+
+autoclass_content = "both"
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +27,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_favicon = "_static/pm.gif"
+html_theme = 'furo'
 html_static_path = ['_static']
